ca9efffced80f157b7bf9d7174ac6bd2f28b94a0
Naufal Aji Wibowo <naufal.aji@mii.co.id>
2019-10-01 14:06:23 +0700

    i add ErrorCode.cs

---
+++ WpfLogin/Models/ErrorCode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfLogin.Models
+{
+    public class ErrorCode
+    {
+        public int Id { get; set; }
+    }
+}
